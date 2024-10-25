2e951468658ba853fdb68a1bd8a2113b8178d73d
MaiHongSon <maihongson151120@gmail.com>
2024-10-25 10:42:01 +0700

    add contract

---
+++ contract/contracts/ConcertFactory.sol
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+import "./Platform.sol";
+
+contract ConcertFactory is Ownable{
+    Platform platform;
+
+    struct TypeTicket{
+        uint price;
+        uint total;
+        uint sold;
+    }
+
+    bool isCancel;
+    uint timeSellStart;
+    uint timeEndConcert;
+    uint fee;
+    mapping (uint => TypeTicket) public typeTicket;
+    mapping (uint ticketId => address) public ticketOwner;
+    mapping (uint ticketId => bool) public isActive;
+
+    event soldTicket( uint ticketId, address ticketOwner );
+    event _transferTicket (address from, address to, uint ticketId);
+
+    constructor(address concertOwner, uint _timeSellStart, uint _timeEndConcert, uint _fee) Ownable(concertOwner){
+        require(fee <= 100, "Fee must be less than 100");
+        require(block.timestamp < _timeSellStart && _timeSellStart < _timeEndConcert  , "Should set the right time");
+        timeSellStart = _timeSellStart;
+        timeEndConcert = _timeEndConcert;
+        platform = Platform(payable(msg.sender));
+        fee = _fee;
+    }
+
+    function setConcertTime(uint _timeSellStart, uint _timeEndConcert) external onlyOwner{
+        require(block.timestamp < _timeSellStart && _timeSellStart < _timeEndConcert  , "Should set the right time");
+        timeSellStart = _timeSellStart;
+        timeEndConcert = _timeEndConcert;
+    }
+
+    function setTicket(uint typeId, uint _price, uint _total) external  onlyOwner {
+        require(0 < _total, "Total ticket must be greater than 0");
+        require(typeTicket[typeId].total == 0, "Type id already exsit");
+        typeTicket[typeId] = TypeTicket(_price, _total, 0);
+    }
+
+    function buyTicket(uint [] calldata typeIds) external payable isNotCancel {
+        // require(timeSellStart <= block.timestamp, "Sale ticket do not start");
+        require(platform.checkUser(msg.sender), "Only platform user can buy ticket");
+        uint totalPurchase = typeIds.length;
+        uint payment;
+        for(uint i; i < totalPurchase; i++)
+        {
+            uint typeId = typeIds[i];
+            // total > sold => total > 0 => type exsit
+            require( typeTicket[typeId].sold < typeTicket[typeId].total, "Do not have enought ticket" );
+            payment += typeTicket[typeId].price;
+            typeTicket[typeId].sold ++;
+            uint ticketId = typeTicket[typeId].sold * 1000 + typeId;
+            ticketOwner[ticketId] = msg.sender;
+            emit soldTicket(ticketId, msg.sender);
+        }
+        require(msg.value == payment,"payment wrong");
+    }
+    function activeTicket(uint ticketId) external isNotActive(ticketId){
+        require(ticketOwner[ticketId] == msg.sender, "Only ticket owner can active ticket");
+        isActive[ticketId] = true;
+    }
+
+    function transferTicket(uint ticketId, address from, address to) external onlyPlatformCall isNotActive(ticketId){
+        require( from == ticketOwner[ticketId], "Ticket must send from owner"); 
+        ticketOwner[ticketId] = to;
+        emit _transferTicket(from, to, ticketId);
+    }
+
+    function cancelConcert() external {
+        require(
+            msg.sender == address(platform) || msg.sender == owner(),
+            "You do not have permission to cancel concert"
+        );
+        isCancel = true;
+    }
+
+    function withdraw() external onlyPlatformCall{
+        require(timeEndConcert < block.timestamp,"Event do not end");
+        uint saleAmount;
+        if(!isCancel){
+            saleAmount = address(this).balance * (100 - fee) / 100;
+            bool sendSale = payable(owner()).send(saleAmount);
+            require(sendSale, "Send failed");
+        }
+        uint feeAmount = address(this).balance - saleAmount;
+        bool sendFee = payable(address(platform)).send(feeAmount);
+        require(sendFee, "Send failed");
+    }
+
+    modifier isNotActive(uint ticketId){
+        require(!isActive[ticketId] , "Ticket already active");
+        _;
+    }
+
+    modifier isNotCancel(){
+        require(!isCancel, "Concert cancelled");
+        _;
+    }
+
+    modifier onlyPlatformCall(){
+        require(
+            msg.sender == address(platform), 
+            "Function only can call by market place"
+        );
+        _;
+    }
+
+}
---
+++ contract/contracts/UserManager.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+import "./ConcertFactory.sol";
+
+contract Platform is AccessControl{
+    struct ListDetail{
+        address ticketOwner;
+        uint listPrice;
+    }
+    mapping (address concert => mapping (uint tokenId => ListDetail)) public listDetail;
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 public constant USER_ROLE = keccak256("USER_ROLE");
+    uint transferfee;
+
+    event newConcert(address concert);
+
+    constructor(){
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(ADMIN_ROLE, msg.sender);
+        _setRoleAdmin(USER_ROLE, ADMIN_ROLE);
+    }
+
+    function deployConcert(address concertOwner, uint timeSellStart, uint timeEndConcert, uint fee) external onlyRole(ADMIN_ROLE) {
+        ConcertFactory concert = new ConcertFactory(concertOwner, timeSellStart, timeEndConcert, fee);
+        emit newConcert(address(concert));
+    }
+
+    function setTranferFee(uint _transferFee) external {
+        require(_transferFee < 100, "Transfer fee must be less than 100");
+        transferfee = _transferFee;
+    }
+
+    function listTicket(address concertAddress, uint ticketId, uint price) external onlyRole(USER_ROLE){
+        require(ConcertFactory(concertAddress).ticketOwner(ticketId) == msg.sender,"Only owner can list ticket");
+        ConcertFactory(concertAddress).transferTicket(ticketId, msg.sender, address(this));
+        listDetail[concertAddress][ticketId] = ListDetail(msg.sender, price);
+    }
+
+    function unlistTicket(address concertAddress, uint ticketId) external onlyRole(USER_ROLE){
+        require(ConcertFactory(concertAddress).ticketOwner(ticketId) == msg.sender,"Only owner can unlist ticket");
+        ConcertFactory(concertAddress).transferTicket(ticketId, address(this), msg.sender);
+        delete (listDetail[concertAddress][ticketId]);
+    }
+
+    function changeListPrice(address concertAddress, uint ticketId, uint newPrice) external onlyRole(USER_ROLE) {
+        require(listDetail[concertAddress][ticketId].ticketOwner == msg.sender,"Only owner can change list price");
+        listDetail[concertAddress][ticketId].listPrice = newPrice;
+    }
+
+    function buyTicket(address concertAddress, uint ticketId) external payable onlyRole(USER_ROLE){
+        address oldOwner = listDetail[concertAddress][ticketId].ticketOwner;
+        require(msg.value == listDetail[concertAddress][ticketId].listPrice,"Payment wrong");
+        delete (listDetail[concertAddress][ticketId]);
+        ConcertFactory(concertAddress).transferTicket(ticketId, address(this), msg.sender);
+        bool sent = payable(oldOwner).send(msg.value * (100 - transferfee) / 100);
+        require(sent, "Fail to send ether");
+    }
+    function checkUser(address account) external view returns(bool){
+        return hasRole(USER_ROLE, account);
+    } 
+
+    function cancelConcert(address concert) external onlyRole(ADMIN_ROLE){
+        ConcertFactory(concert).cancelConcert();
+    }
+    function withdrawConcert(address concert) external onlyRole(ADMIN_ROLE){
+        ConcertFactory(concert).withdraw();
+    }
+
+    function getBlockTime() external view returns(uint time){
+        time = block.timestamp;
+    }
+
+    function withdrawPlatformFee() external onlyRole(DEFAULT_ADMIN_ROLE){
+        bool success = payable(msg.sender).send(address(this).balance);
+        require(success,"Send fail");
+    }
+    
+    receive() external payable { }
+}
